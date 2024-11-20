f4eb3cd91cf4ad753ff54f7958c354795ebcc7e0
Christopher Adams <christopher.m.adams2@gmail.com>
2024-11-20 10:17:23 -0800

    refactored in a function for funsies

--- listComprehension.py
+++ listComprehension.py
@@ -1,17 +1,26 @@
 # Write a Python Program to find out common letters between two strings
 
 # test case
-string1 = 'NAINA'
-string2 = 'REENE'
+# string1 = 'NAINA'
+# string2 = 'REENE'
 
 # 1) Create a list from each string
 # 2) Set the list so it is unique
 # 3) List comprehension to get common letters
 
-set1 = set(string1)
-set2 = set(string2)
+# set1 = set(string1)
+# set2 = set(string2)
 
-commonLetters = {x for x in set1 if x in set2}
-print(set1)
-print(set2)
-print(commonLetters)
+# commonLetters = {x for x in set1 if x in set2}
+# print(set1)
+# print(set2)
+# print(commonLetters)
+
+def getCommonLetters(string1: str, string2: str) -> set:
+    return {x for x in set(string1.lower()) if x in set(string2.lower())}
+
+string1: str = input("Enter first string: ")
+string2: str = input("Enter second string: ")
+
+commonLetters: set = getCommonLetters(string1, string2)
+print("Common Letters: " + str(commonLetters))
